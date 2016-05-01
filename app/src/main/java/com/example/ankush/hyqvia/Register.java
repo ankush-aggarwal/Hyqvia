850fdaa69bbb0d75e4c7a4ad1e3fbec9622a2178
Ankush Aggarwal <ankush.aggarwl@gmail.com>
2016-05-01 00:16:29 -0500

    source code

---
+++ app/src/main/java/com/example/ankush/hyqvia/Register.java
@@ -0,0 +1,16 @@
+package com.example.ankush.hyqvia;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ankush on 4/30/16.
+ */
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+}
